23868cab411ad4560439b92d43d2aa1aa8804652
Anderson Mauricio <andersonjm_2005@hotmail.com>
2020-05-01 20:18:35 -0300

    Classe Program pronta

--- ExFix/Program.cs
+++ ExFix/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExFix.Entities;
+using ExFix.Entities.Enums;
+using System;
+using System.Globalization;
 
 namespace ExFix
 {
@@ -6,7 +9,49 @@ namespace ExFix
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
 
+            Client client = new Client(name, email, birthDate);
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{1} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(name, price);
+                OrderItem item = new OrderItem(quantity, product.Price, product);
+
+                order.AddItem(item);
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+            Console.ReadLine();
         }
     }
 }
